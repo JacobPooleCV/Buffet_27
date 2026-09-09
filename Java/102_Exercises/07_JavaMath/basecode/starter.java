a9bf0519d6319ac83c9571ae90cddd6ff9ea900a
Svar-lang <svarsantoro@gmail.com>
2026-09-09 18:11:50 +0000

    update

--- Java/102_Exercises/07_JavaMath/basecode/starter.java
+++ Java/102_Exercises/07_JavaMath/basecode/starter.java
@@ -10,10 +10,10 @@ class starter {
         // ===================================================
  
         // TODO: Construct and declare an int variable named applesPicked and set it to 12
-        
+        int applesPicked = 12;
  
         // TODO: Construct and declare a double variable named boxWeightLbs and set it to 2.5
-        
+        double boxWeightLbs = 2.5;
  
         System.out.println("Apples picked: " + applesPicked);
         System.out.println("Box weight: " + boxWeightLbs);
@@ -29,7 +29,8 @@ class starter {
  
         // TODO: Construct and declare an int variable named totalBaskets that equals
         // applesPicked multiplied by applesPerBasket
-        
+        int totalBaskets = applesPicked * applesPerBasket
+    
  
         System.out.println("Total baskets: " + totalBaskets);
         // Expected: Total baskets: 48
@@ -46,8 +47,8 @@ class starter {
  
         // TODO: Construct and declare an int variable named cookiesPerStudentInt that
         // divides totalCookies by studentsInClass using INTEGER division
-        
- 
+        int cookiesPerStudentInt totalCookies / studentsInClass
+  
         System.out.println("Cookies per student (int division): " + cookiesPerStudentInt);
         // Expected: Cookies per student (int division): 3
         // (17 / 5 = 3.4, but int division truncates to 3)
@@ -56,7 +57,7 @@ class starter {
         // divides totalCookies by studentsInClass using DOUBLE (decimal) division.
         // Hint: you'll need to cast at least one of the ints to a double,
         // e.g. (double) totalCookies or add/multiple by 0.0/1.0
-        
+        double cookiesPerStudentDouble (double)totalCookies / studentsInClass; 
  
         System.out.println("Cookies per student (double division): " + cookiesPerStudentDouble);
         // Expected: Cookies per student (double division): 3.4
@@ -69,7 +70,7 @@ class starter {
  
         // TODO: Construct and declare an int variable named leftoverCookies that stores
         // the remainder of totalCookies divided by studentsInClass
-        
+        int leftoverCookies totalCookies(double) / studentsInClass
  
         System.out.println("Leftover cookies: " + leftoverCookies);
         // Expected: Leftover cookies: 2
