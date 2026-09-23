3b39d022c294040861d53550336e63b5cc2b313e
Svar-lang <svarsantoro@gmail.com>
2026-09-23 18:07:06 +0000

    update

--- Java/102_Exercises/12_Colors/base_code/starter.java
+++ Java/102_Exercises/12_Colors/base_code/starter.java
@@ -8,8 +8,17 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		getColor(100, 90, 240);
+        int r = (int) (Math.random() * 255);
+        int g = (int) (Math.random() * 255);
+        int b = (int) (Math.random() * 255);
+        getColor(r, g, b);
+        getColor(255-r, 255-g, 255-b);
+        
 
+
+		getColor(b, g, b);
+        getColor(g, r, b);
+        
 		// Call getColor(#, #, #);
 	}
 
