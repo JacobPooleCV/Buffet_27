21f53ee8a99acb79a32edd081fc243a0c23ad408
Svar-lang <svarsantoro@gmail.com>
2026-09-24 17:58:15 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -10,9 +10,7 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		Scanner sc = new Scanner(System.in) ;
-		System.out.print( Math.random() );
-		int x = (int) Math.random() * 90 ;
+		
 		
 	
 	
