5b827617cda5174fee9f1ff6faded35e948060c3
Svar-lang <svarsantoro@gmail.com>
2026-09-24 18:16:09 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,19 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
+		String happy =new String("Happy");
+		String birthday=new String("birthday");
+		String Svar =new String("To Svar");
+		System.out.println("Happy Birthday to you");
+		
+		
 		System.out.print("I love to learn coding remotely."); 
+		
+	
+	
+	
+	
+	
+	
 	}
 }
