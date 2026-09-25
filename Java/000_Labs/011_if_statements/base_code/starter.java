13e5e1b0aa9f3738d81c55f95c6deb9ed93c552d
Svar-lang <svarsantoro@gmail.com>
2026-09-25 17:58:25 +0000

    update

--- Java/000_Labs/011_if_statements/base_code/starter.java
+++ Java/000_Labs/011_if_statements/base_code/starter.java
@@ -8,10 +8,7 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.println()
-		
-		
+		// the command window when you compile and run this program
 		System.out.print("I love to learn coding remotely.");
 		
 
